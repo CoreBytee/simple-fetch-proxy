21feccd48e3be4dd0cf428f5263dd048478874fe
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-12-05 16:05:31 +0100

    continue working

--- packages/proxy-client/src/index.ts
+++ packages/proxy-client/src/index.ts
@@ -3,21 +3,28 @@ export default function createFetch(
         ssl: boolean,
         host: string,
         port: number,
+        secret: string,
     } = {
             ssl: false,
             host: 'localhost',
             port: 4000,
+            secret: process.env.PROXY_SECRET as string
         }
 ): Function {
     const proxyUrl = `${options.ssl ? 'https' : 'http'}://${options.host}:${options.port}/`
 
     return async function (
         url: string,
-        options: RequestInit = {}
+        fetchOptions: RequestInit = {}
     ): Promise<Response> {
+        const fetchUrl = new URL(proxyUrl)
+        fetchUrl.searchParams.set('url', url)
+        fetchUrl.searchParams.set("method", fetchOptions.method || "GET")
+        fetchUrl.searchParams.set("headers", JSON.stringify(fetchOptions.headers || {}))
+        fetchUrl.searchParams.set("secret", options.secret)
+
         const response = await fetch(
-            `${proxyUrl}?${url}`,
-            options
+            fetchUrl.toString()
         )
 
         return response
--- packages/proxy-server/src/index.ts
+++ packages/proxy-server/src/index.ts
@@ -8,19 +8,44 @@ const app = express()
 app.all(
     "/",
     async (request, response) => {
-        console.log(request.url.substring(2), request)
+        const secret: string = request.query.secret as string
+        const url: string = request.query.url as string
+        const headers: string = request.query.headers as string
+        const method: string = request.method
+
+        if (secret !== process.env.PROXY_SECRET) {
+            response.status(401).send("Unauthorized")
+        }
+
+        console.log(
+            `Request: ${method} ${url} ${headers}`
+        )
 
         const fetchResponse = await fetch(
-            request.url.substring(2),
+            url,
             {
-                headers: request.headers as object,
-                verbose: true
+                method,
+                headers: JSON.parse(headers),
+                body: request.body
             }
         )
 
-        response.send(
-            await fetchResponse.text()
-        )
+        function copyHeader(headerName: string) {
+            const headerValue = fetchResponse.headers.get(headerName)
+            if (headerValue) {
+                response.setHeader(headerName, headerValue)
+            }
+        }
+
+        response.status(fetchResponse.status)
+        copyHeader("Content-Type")
+        copyHeader("Content-Length")
+        copyHeader("Content-Disposition")
+        copyHeader("Content-Language")
+        copyHeader("Content-Range")
+        copyHeader("accept-ranges")
+        response.send(await fetchResponse.text())
+
     }
 )
 
